87da316a8e244bd63dd25bc97b156e9f12ff1d6a
Andressa Silva <andressacavalcante.ti@gamil.com>
2025-03-18 13:39:03 -0300

    Adicionar atualização da lista na tela

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ let sorteados = [];
 function adicionarAmigo() {
     const inputAmigo = document.getElementById('amigo');
     const nomeAmigo = inputAmigo.value.trim();
+
+    if (!nomeAmigo) {
+        alert("Insira o nome de um amigo.");
+        inputAmigo.focus();
+        return;
+}
+
+    if (amigos.includes(nomeAmigo)) {
+    alert(`O nome "${nomeAmigo}" já foi adicionado. Insira um novo nome!`);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    return;
+    }
+
+    amigos.push(nomeAmigo);
+    atualizarListaAmigos();
+    inputAmigo.value = "";
+    inputAmigo.focus();
+}
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
